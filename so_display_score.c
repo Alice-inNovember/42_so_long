8dda464b7ef8f32b4b300337b3d3d09bf2c82fc1
Junseo Lee <junlee2@c8r7s2.42seoul.kr>
2022-10-17 16:42:09 +0900

    2022/10/17 - 16:42:09 backup

--- so_display_score.c
+++ so_display_score.c
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:15:44 by junlee2           #+#    #+#             */
-/*   Updated: 2022/10/17 16:41:04 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2022/10/17 16:42:07 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	font_init(t_data *data)
 {
 	(void)data;
+	https://www.google.com/search?client=firefox-b-d&q=%3A
 }
 
 void	display_score(t_data *data)
